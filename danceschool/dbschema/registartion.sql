36274e9b577d20e7d3f2a70811488b73d5106179
Browse <browse@veenasmacbookpro.local>
2020-06-27 17:42:51 -0700

    dsproject completed

--- danceschool/dbschema/registartion.sql
+++ danceschool/dbschema/registartion.sql
@@ -1,8 +1,10 @@
-create table contact_details(fname varchar(30),lname varchar(30),dob text,contactno int(10),email varchar(50));
+create table contact_details(fname varchar(30),lname varchar(30),dob text,contactno int(10),email varchar(50),skilllevel text,comments text);
+
+ALTER TABLE contact_details ADD comments text ;
+AlTER TABLE contact_details ADD skilllevel text ;
 
 
+INSERT INTO contact_details (fname, lname, dob, contactno, email,comments) VALUES ("Nihira", "Nidish", "2016-01-23", 2019170864,"snidishc@gmail.com","hello" );
+INSERT INTO contact_details (fname, lname, dob, contactno, email,comments) VALUES ("Nirmaya", "Nidish", "2009-11-25", 2019170864,"snidishc@gmail.com" ,"how are u");
 
-
-INSERT INTO contact_details (fname, lname, dob, contactno, email) VALUES ("Nihira", "Nidish", "2016-01-23", 2019170864,"snidishc@gmail.com" );
-INSERT INTO contact_details (fname, lname, dob, contactno, email) VALUES ("Nirmaya", "Nidish", "2009-11-25", 2019170864,"snidishc@gmail.com" );
-
+INSERT INTO contact_details(fname, lname, dob, contactno, email,comments) VALUES ("maya", "Nidish", "2020-11-25", 2019170864,"snc@gmail.com" ,"how are u");
--- danceschool/registration.php
+++ danceschool/registration.php
@@ -1,4 +1,6 @@
 <?php
+
+
 $fname=$_POST["fname"];
 echo "fname: " . $fname;
 echo "<br>";
@@ -12,13 +14,61 @@ $contactno = $_POST["contactno"];
 echo "contactno: " . $contactno;
 echo "<br>";
 $email = htmlspecialchars($_POST["email"]);
-echo " email:" . $email;
+echo " email:" . $email ;
+echo "<br>";
+
+$skilllevel = $_POST['skilllevel'];  // Storing Selected Value In Variable
+echo "You have selected :" .$skilllevel;  // Displaying Selected Value
+echo"<br>";
+
+$comments = htmlspecialchars($_POST["comments"]);
+echo " comments:" . $comments ;
+
+
+
 
 
 
 #echo "INSERT INTO contact_details (fname, lname, dob, contactno, email) VALUES ($fname, $lname, $dob, $contactno, $email)";
-
+//db down temperory commented
 $db = new SQLite3('/Users/browse/Sites/danceschool/danceschool.db');
-$db->exec("INSERT INTO contact_details (fname, lname, dob, contactno, email) VALUES ('$fname', '$lname', '$dob', $contactno, '$email')");   
+$db->exec("INSERT INTO contact_details (fname, lname, dob, contactno, email,skilllevel,comments) VALUES ('$fname', '$lname', '$dob', '$contactno', '$email','$skilllevel','$comments')");  
+
+
+
+
+
+
+
+
+
+
+/*
+    $beginner_level = 'unchecked';
+    $intermediate_level = 'unchecked';
+    $senior_level="senior";
+
+ if (isset($_POST['Submit'])) 
+    {
+         $selected_radio = $_POST[''];
+
+         if ($selected_radio == 'beginner') 
+             {
+                $beginner_level = 'checked';
+             }
+          elseif ($selected_radio == 'intermediate') 
+          {
+                $intermediate_level = 'checked';
+          }
+          else 
+          {
+                $senior_level = 'checked';
+          }
+    }*/
+
+
+
+
+
 
 ?>
